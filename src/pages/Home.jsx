2cdd13a254965c3addf09940c4a151f9cc9c1637
LiyandPeter <168171761+LiyandPeter@users.noreply.github.com>
2025-03-17 15:31:22 +0800

    change3/17_03-31

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import AuthContainer from '../componemts/AuthContainer'
 import Header from '../componemts/Header'
+import LoginForm from '../componemts/Login'
+import NavLink from '../componemts/NavLink'
+import RegisterForm from '../componemts/Regist'
 
 
 function Home(){
 
     return(
         <div>
+            Hello world
             <Header/>
             <AuthContainer/>
-           
+            <LoginForm/>
+            <NavLink/>
+            <RegisterForm/>
         </div>
     )
 }
