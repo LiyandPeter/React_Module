4f90dc05655601baae48e6342185daf1343cdfd1
LiyandPeter <168171761+LiyandPeter@users.noreply.github.com>
2025-03-17 13:39:40 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import AuthContainer from './components/AuthContainer';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <Header />
-      <AuthContainer />
+      <main>
+        <AuthContainer />
+      </main>
     </>
   );
 }
